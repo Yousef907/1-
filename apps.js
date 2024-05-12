c8d89766257e399cd8098d3ba375ccd1f9ba201d
Yousef907 <y.z9974@gmail.com>
2024-05-12 11:57:23 +0300

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,6 @@
+function sendData() {
+    var input1 = document.getElementById('input1').value;
+    var response = document.getElementById('response');
+
+    response.innerText = "You entered: " + input1 + " and " + input2;
+}
